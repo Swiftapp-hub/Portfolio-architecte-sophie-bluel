23d3093b7fb1159cd96dda789adfde4ee272ce51
SwiftApp <swiftyassistant.io@gmail.com>
2023-03-10 15:11:06 +0100

    Add categories filters

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,7 +1,8 @@
 /*
  * Global variables
  */
-var galleryItem;
+const galleryItem = [];
+const categories = [];
 
 /*
  * Events listener
@@ -19,22 +20,30 @@ document.addEventListener("DOMContentLoaded", function () {
  * Functions for gallery items
  */
 function refresh(isPopup, items = null, category = -1) {
+    // Delete item in gallery div
     const rootEL = document.getElementById('gallery-list');
     while (rootEL.firstChild) rootEL.removeChild(rootEL.lastChild);
 
     if (!isPopup) {
         if (category === -1) {
-            if (items === null) galleryItem.forEach((item) => addItemInGallery(item))
+            // Refresh without new data
+            if (items === null) galleryItem.forEach((item) => addItemInGallery(item, true))
+            // Refresh with new data from DB
             else {
-                galleryItem = [];
-                items.forEach((item) => addItemInGallery(item));
+                addCategory({id: -1, name: 'Tous'}, true);
+                items.forEach((item, index) => {
+                    addItemInGallery(item);
+                    if (index < 3) addCategory(item.category);
+                });
             }
         } else {
+            // Refresh with category filter
             galleryItem.forEach((item) => {
                 if (item.categoryId === category) addItemInGallery(item, true);
             })
         }
     } else {
+        // Refresh edit popup
         galleryItem.forEach((item) => addItemInGalleryPopup(item))
     }
 }
@@ -57,4 +66,30 @@ function addItemInGallery(item, isWithCategory = false) {
 }
 
 function addItemInGalleryPopup(item) {
+}
+
+/*
+ * Functions for categories
+ */
+function addCategory(category, checked = false) {
+    categories.push(category);
+
+    const input = document.createElement('input');
+    const label = document.createElement('label');
+
+    input.setAttribute('type', 'radio');
+    input.setAttribute('name', 'filters');
+    input.setAttribute('value', category.id);
+    input.setAttribute('id', category.name);
+    if (checked) input.checked = true;
+
+    input.addEventListener('change', (e) => {
+        refresh(false, null, Number(e.target.value));
+    })
+
+    label.setAttribute('for', category.name);
+    label.textContent = category.name;
+
+    document.getElementById('categories').appendChild(input);
+    document.getElementById('categories').appendChild(label);
 }
